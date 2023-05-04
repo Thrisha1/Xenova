c580456d62b29af65614fa76f4747d29e0837eaa
Vaishnav <vaishnavvaichu123@gmail.com>
2023-05-04 20:48:59 +0530

    graphQL

---
+++ src/functions/auth/token.js
@@ -0,0 +1,53 @@
+import jwtDecode from "jwt-decode"
+class Token {
+  constructor(type) {
+    this.type = type
+  }
+  getExpiry = token => {
+    try {
+      console.log(token)
+      let tokenDecoded = jwtDecode(token)
+      let exp = tokenDecoded.exp
+      let expiry = new Date().valueOf() - new Date(exp * 1000).valueOf()
+
+      ///consollleee
+      console.log(expiry)
+      return expiry
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  isValid() {
+    let token = this.get()
+    if (token === null) {
+      return false
+    }
+
+    let expiry = this.getExpiry(token)
+    if (expiry <= 0) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  get = () => {
+    return localStorage.getItem(this.type)
+  }
+
+  getData = () => {
+    let token = this.get()
+    if (token) return jwtDecode(token)
+    else return null
+  }
+
+  set = token => {
+    localStorage.setItem(this.type, token)
+  }
+
+  remove = () => {
+    localStorage.removeItem(this.type)
+  }
+}
+export default Token
---
+++ src/functions/graphql/apolloClient.js
@@ -0,0 +1,108 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloLink,
+  HttpLink,
+  from,
+  fromPromise
+} from "@apollo/client"
+import { onError } from "@apollo/client/link/error"
+import Token from "../auth/token"
+import { RetryLink } from "@apollo/client/link/retry"
+import auth from "../auth"
+import { offsetLimitPagination } from "@apollo/client/utilities"
+
+const token = new Token("id")
+
+const retryLink = new RetryLink({
+  delay: {
+    initial: 300,
+    max: Infinity,
+    jitter: true
+  },
+  attempts: {
+    max: 5,
+    retryIf: (error, _operation) => {
+      console.log(error)
+      return false
+    }
+  }
+})
+
+const httpLink = new HttpLink({
+  uri: "https://localhost:8080/v1/graphql"
+})
+
+const authLink = new ApolloLink((operation, forward) => {
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      ...(!token.get()
+        ? { "X-Hasura-Role": "anonymous" }
+        : { Authorization: `Bearer ${token.get()}` || null })
+    }
+  }))
+
+  return forward(operation)
+})
+
+const errorLink = onError(
+  ({ networkError, graphQLErrors, response, forward, operation }) => {
+    if (networkError && networkError.statusCode === 401) {
+    }
+    if (graphQLErrors) {
+      graphQLErrors.map(err => {
+        const {
+          message,
+          extensions: { code },
+          path
+        } = err
+        console.log(err)
+        if (code === "invalid-jwt")
+          return fromPromise(
+            new Promise((res, rej) => {
+              auth.refreshSession(err => {
+                if (!err) res(true)
+                else res(false)
+              })
+            })
+          )
+            .filter(x => {
+              console.log(x)
+              return !!x
+            })
+            .flatMap(() => {
+              const oldHeaders = operation.getContext().headers
+              console.log(token.get())
+              // modify the operation context with a new token
+              operation.setContext({
+                headers: {
+                  ...oldHeaders,
+                  Authorization: `Bearer ${token.get()}`
+                }
+              })
+              return forward(operation)
+            })
+      })
+    }
+    // if (response) response.errors = undefined;
+  }
+)
+
+const graphQLClient = () =>
+  new ApolloClient({
+    cache: new InMemoryCache({
+      typePolicies: {
+        Query: {
+          fields: {
+            show_nearby_properties: offsetLimitPagination(),
+            search_property: offsetLimitPagination(),
+            property: offsetLimitPagination()
+          }
+        }
+      }
+    }),
+    link: from([authLink, errorLink, httpLink])
+  })
+
+export default graphQLClient
