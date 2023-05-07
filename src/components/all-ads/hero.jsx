71826618101a1e95476f2bc3503ce73bcd90274e
Muhammed rinshad s r <81734540+DareDevilStudios@users.noreply.github.com>
2023-05-07 20:38:14 +0530

    Link changed to Onclick fn

--- src/components/all-ads/hero.jsx
+++ src/components/all-ads/hero.jsx
@@ -20,9 +20,18 @@ const Hero = () => {
                     </div>
 
                     {
-                        adShow.map((ad) => (
-                            <Ad_card key={ad.id} imageSrc={ad.image} desc={ad.description} Amt={ad.amt} link={ad.link} />
-                        ))
+                        adShow.length > 0 ? (
+                            adShow.map((ad) => (
+                                <Ad_card key={ad.id} imageSrc={ad.image} desc={ad.description} Amt={ad.amt} link={ad.link} />
+                            ))
+                        ) : (
+
+                            
+                            <div className="md:w-1/2 flex-col">
+                                <h1 className="text-white text-2xl font-medium">No ads currently available</h1>
+                            </div>
+
+                        )
                     }
 
                 </div>
