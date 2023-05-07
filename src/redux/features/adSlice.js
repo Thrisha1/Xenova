0435f42e3b7ca9393f504b38b462310f839d72e5
Muhammed rinshad s r <81734540+DareDevilStudios@users.noreply.github.com>
2023-05-07 17:25:22 +0530

    conflict merged till shared ads

--- src/redux/features/adSlice.js
+++ src/redux/features/adSlice.js
@@ -14,7 +14,7 @@ const initialState = {
     {
         id: 2,
         title: "Ad 2",
-        des//imgcription: 'Eid is a digital advertising platform that helps you find the right ad for your business.',
+        description: 'Eid is a digital advertising platform that helps you find the right ad for your business.',
         image: "https:.freepik.com/free-vector/realistic-eid-mubarak-greeting-card_1017-31239.jpg?w=2000",
         amt: 200,
         link:"https://www.google.co.in/"
