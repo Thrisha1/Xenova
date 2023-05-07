8f0021a36a90ab2a472ef8e0d8a036c388693b3f
Muhammed rinshad s r <81734540+DareDevilStudios@users.noreply.github.com>
2023-05-07 17:10:27 +0530

    ads page completed and linked with Redux and need to complete the shared ads

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./features/counterSlice";
+import adReducer from "./features/adSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    ad: adReducer,
   },
   devTools: process.env.NODE_ENV !== "production",
 });
